7141d8d2a198e86a7a760eba62f1ca2a7f3bb6a9
Pelumi <saplos123456@gmail.com>
2019-06-23 14:13:52 +0100

    A commit

---
+++ voting.py
@@ -0,0 +1,20 @@
+"""This simple voting app is used in the 2019 presidential elections"""
+
+Atiku=0
+Buhari=0
+status=False
+
+def vote():
+    global Atiku,Buhari,status
+    entry= int(input("Welcome to the voting app \n \nFor Atiku Abubakar: 1 \n \nFor Muhammadu Buhari: 2\n \nTo exit: 3 \n"))
+    if entry == 1:
+        Atiku += 1
+    elif entry == 2:
+        Buhari +=1
+    elif entry == 3:
+        status = True
+
+while status == False:
+    vote()
+
+print('The summary of the results are as follows: \n \nAtiku Abubakar: {0} \n \nMuhammadu Buhari: {1}'.format(Atiku,Buhari))
