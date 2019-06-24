15ad2e00339e0b50ad1e92373f736251d3a07946
Pelumi <saplos123456@gmail.com>
2019-06-24 10:38:42 +0100

    This should got now yh

--- voting.py
+++ voting.py
@@ -1,20 +1,27 @@
 """This simple voting app is used in the 2019 presidential elections"""
 
-Atiku=0
-Buhari=0
-status=False
+Atiku = 0
+Buhari = 0
+status = False
+
 
 def vote():
-    global Atiku,Buhari,status
-    entry= int(input("Welcome to the voting app \n \nFor Atiku Abubakar: 1 \n \nFor Muhammadu Buhari: 2\n \nTo exit: 3 \n"))
+    global Atiku, Buhari, status
+    entry = int(input("Welcome to the voting app \n \
+        \nFor Atiku Abubakar: 1 \n \
+        \nFor Muhammadu Buhari: 2\n \
+        \nTo exit: 3 \n"))
     if entry == 1:
         Atiku += 1
     elif entry == 2:
-        Buhari +=1
+        Buhari += 1
     elif entry == 3:
         status = True
 
-while status == False:
+
+while status is False:
     vote()
 
-print('The summary of the results are as follows: \n \nAtiku Abubakar: {0} \n \nMuhammadu Buhari: {1}'.format(Atiku,Buhari))
+print('The summary of the results are as follows: \n \
+    \nAtiku Abubakar: {0} \n \
+    \nMuhammadu Buhari: {1}'.format(Atiku, Buhari))
